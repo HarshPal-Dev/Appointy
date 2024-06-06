c55e3205fde8a0557ed067183d4746eedc06b7af
HarshPal-Dev <harsh191005@gmail.com>
2024-06-06 12:00:00 +0530

    Add dashboard charts in admin panel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,3 +14,4 @@ const userSchema = new mongoose.Schema({
 const userModel = mongoose.models.user || mongoose.model("user", userSchema);
 export default userModel;// Update on 2024-06-02 - Commit 6
 // Update on 2024-06-05 - Commit 4
+// Update on 2024-06-06 - Commit 1
