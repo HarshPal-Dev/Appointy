669a3d1e2e3bad4701585ca9cba2a3469ab9703b
HarshPal-Dev <harsh191005@gmail.com>
2024-07-04 12:00:00 +0530

    Update dependencies in package.json

--- backend/server.js
+++ backend/server.js
@@ -32,3 +32,4 @@ app.listen(port, () => console.log(`Server started on PORT:${port}`))
 // Update on 2024-06-15 - Commit 5
 // Update on 2024-06-16 - Commit 4
 // Update on 2024-06-29 - Commit 1
+// Update on 2024-07-04 - Commit 3
