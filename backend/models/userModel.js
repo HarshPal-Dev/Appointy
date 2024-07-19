28c18bf6031ba52bec153ce755f63cd9d34c0d90
HarshPal-Dev <harsh191005@gmail.com>
2024-07-19 12:00:00 +0530

    Improve dark mode styles in frontend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -22,3 +22,4 @@ export default userModel;// Update on 2024-06-02 - Commit 6
 // Update on 2024-07-03 - Commit 3
 // Update on 2024-07-13 - Commit 4
 // Update on 2024-07-17 - Commit 4
+// Update on 2024-07-19 - Commit 2
