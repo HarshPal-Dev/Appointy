ca06cbd009c0a793095e05a2c5135706aa3f0f46
HarshPal-Dev <harsh191005@gmail.com>
2024-08-07 12:00:00 +0530

    Fix minor typo in README.md

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -24,3 +24,4 @@ export default userModel;// Update on 2024-06-02 - Commit 6
 // Update on 2024-07-17 - Commit 4
 // Update on 2024-07-19 - Commit 2
 // Update on 2024-08-04 - Commit 2
+// Update on 2024-08-07 - Commit 2
