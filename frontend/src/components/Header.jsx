58f6786a1ead2bd35e1589cba5e2e75905850428
HarshPal-Dev <harsh191005@gmail.com>
2024-08-13 12:00:00 +0530

    Update .gitattributes for better cross-platform support

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -33,3 +33,4 @@ export default Header// Update on 2024-06-02 - Commit 4
 // Update on 2024-07-20 - Commit 1
 // Update on 2024-07-27 - Commit 5
 // Update on 2024-08-01 - Commit 1
+// Update on 2024-08-13 - Commit 4
