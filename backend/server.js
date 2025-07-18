3a27bd68f753ace461921761501ad44c555dd8dd
HarshPal-Dev <harsh191005@gmail.com>
2025-07-18 10:10:56 +0530

    Improve error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -39,3 +39,4 @@ app.listen(port, () => console.log(`Server started on PORT:${port}`))
 // Update on 2024-07-27 - Commit 4
 // Update on 2024-08-03 - Commit 4
 // Update on 2024-08-06 - Commit 3
+// Update on 2025-07-18 - Commit 2
