52487dc841c0891b89999f05506146536bd124c9
HarshPal-Dev <harsh191005@gmail.com>
2025-07-28 15:39:34 +0530

    Bugfix: resolve JWT expiration issues

--- backend/server.js
+++ backend/server.js
@@ -41,3 +41,4 @@ app.listen(port, () => console.log(`Server started on PORT:${port}`))
 // Update on 2024-08-06 - Commit 3
 // Update on 2025-07-18 - Commit 2
 // Update on 2025-07-28 - Commit 1
+// Update on 2025-07-28 - Commit 2
