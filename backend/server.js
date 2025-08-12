7bad4fde9f143df436f702a7837d7964531275f9
HarshPal-Dev <harsh191005@gmail.com>
2025-08-12 14:12:28 +0530

    Improve authentication in admin panel

--- backend/server.js
+++ backend/server.js
@@ -44,3 +44,4 @@ app.listen(port, () => console.log(`Server started on PORT:${port}`))
 // Update on 2025-07-28 - Commit 2
 // Update on 2025-07-30 - Commit 2
 // Update on 2025-08-06 - Commit 2
+// Update on 2025-08-12 - Commit 2
